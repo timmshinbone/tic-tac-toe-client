e3f2dc7ccbaf5ef006665b806400c36f38651c93
cuddybrisp <cuddybrisp@gmail.com>
2020-07-16 11:39:17 -0500

    Plugged url for tictactoe
    curl scripts updated, submit auth events working

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,7 +1,7 @@
 # VARIABLE=VALUE sh curl-scripts/auth/sign-up.sh
 
 # don't use a password you use for any real websites!
-curl "https://library-express-api.herokuapp.com/sign-up" \
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-up" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
